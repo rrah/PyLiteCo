39b42a4764730f115bd5a5d0311e3b7e78b40d15
Robert Walker <rw776@york.ac.uk>
2015-03-19 11:03:11 +0000

    can connect to box

--- __main__.py
+++ __main__.py
@@ -7,6 +7,10 @@ import logging
 import echo360.capture_device as echo
 
 with open('config.json') as CONFIG_FILE:
-    CONFIG = json.loads(CONFIG)
+    CONFIG = json.load(CONFIG_FILE)
 
 device = echo.Echo360CaptureDevice(CONFIG['server'], CONFIG['user'], CONFIG['pass'])
+if not device.connection_test.success():
+    print('Something went wrong connecting')
+
+print(device.status_current_capture())
