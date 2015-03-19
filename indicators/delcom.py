17b4ea19860c3fc97c202203bf7462cd2b6014ff
Robert Walker <rw776@york.ac.uk>
2015-03-19 11:58:22 +0000

    prepare to deal with failed attempts to change light

--- indicators/delcom.py
+++ indicators/delcom.py
@@ -11,18 +11,18 @@ import subprocess
 
 class DelcomGen2():
     def __init__(self):
-        self.exe = 'control_delcom_gen2.exe'
+        self.exe = '../control_delcom_gen2.exe'
         self.allowed_colours = {'green': 'green', 'yellow': 'blue', 'red': 'red', 'off': 'off'}
 
     def set_light(self, colour):
         if colour not in self.allowed_colours:
             raise Exception('Not allowed colour')
-        subprocess.call([self.exe, '--' + self.allowed_colours[colour]])
+        subprocess.check_call([self.exe, '--' + self.allowed_colours[colour]])
 
 
 if __name__ == '__main__':
     from time import sleep
     device = DelcomGen2()
-    device.set_light('red')
+    device.set_light('off')
     sleep(1)
     device.set_light('off')
