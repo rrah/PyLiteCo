fdd2bc9a693b7e3c30df6605400e96e874065062
Robert Walker <rw776@york.ac.uk>
2015-03-19 17:14:44 +0000

    Now does some checking

--- __main__.py
+++ __main__.py
@@ -1,28 +1,47 @@
 #!/usr/bin/python
 
+
 import json
+import sys
+from time import sleep
 
 import logging
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+root.addHandler(ch)
+
 
 import echo360.capture_device as echo
-
 import indicators.delcom as delcom
 
+
 from echoip import ECHO_URL
+
 
 with open('config.json') as CONFIG_FILE:
     CONFIG = json.load(CONFIG_FILE)
 
-echo_device = echo.Echo360CaptureDevice(ECHO_URL, CONFIG['user'], CONFIG['pass'])
-if not echo_device.connection_test.success():
-    print('Something went wrong connecting')
-else:
-    print(echo_device.capture_status_str())
-
-
-from time import sleep
-
 indi_device = delcom.DelcomGen2()
-indi_device.set_light_red()
-sleep(1)
-indi_device.set_light_off()
+echo_device = echo.Echo360CaptureDevice(ECHO_URL, CONFIG['user'], CONFIG['pass'])
+
+def check_status():
+    if not echo_device.connection_test.success():
+        logging.error('Something went wrong connecting')
+    else:
+        state = [thing.split('=')[1] for thing in echo_device.capture_status_str().split(';') if 'State' in thing][0]
+        if state == 'inactive':
+            indi_device.set_light_green()
+        elif state == 'active':
+            indi_device.set_light_yellow()
+        else:
+            indi_device.set_light_red()
+try:
+    while True:
+        check_status()
+        sleep(0.5)
+except KeyboardInterrupt:
+    pass
