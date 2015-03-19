1aa2b7cf3c20e0fa22580d77393e91cb6d169757
Robert Walker <robert.walker@ystv.co.uk>
2015-03-19 12:42:31 +0000

    ignore pyc files

--- __main__.py
+++ __main__.py
@@ -6,11 +6,15 @@ import logging
 
 import echo360.capture_device as echo
 
+import indicators.delcom as delcom
+
 with open('config.json') as CONFIG_FILE:
     CONFIG = json.load(CONFIG_FILE)
 
-device = echo.Echo360CaptureDevice(CONFIG['server'], CONFIG['user'], CONFIG['pass'])
-if not device.connection_test.success():
+print CONFIG
+
+echo_device = echo.Echo360CaptureDevice(CONFIG['server'], CONFIG['user'], CONFIG['pass'])
+if not echo_device.connection_test.success():
     print('Something went wrong connecting')
 
-print(device.status_current_capture())
+print(echo_device.status_system())
