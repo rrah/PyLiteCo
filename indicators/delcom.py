67abc66be9af7ea99ea24b57d0a930fe216b31be
AV Staff <avstaff@raspberrypi.(none)>
2015-03-19 11:18:30 +0000

    start of device control

---
+++ indicators/delcom.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+"""
+Module for delcom status lights.
+
+Author: Robert Walker <rw776@york.ac.uk> 19/03/15
+"""
+
+import subprocess
+
+
+class DelcomGen2():
+    def __init__(self):
+        self.exe = 'control_delcom_gen2.exe'
+        self.allowed_colours = {'green': 'green', 'yellow': 'blue', 'red': 'red', 'off': 'off'}
+
+    def set_light(self, colour):
+        if colour not in self.allowed_colours:
+            raise Exception('Not allowed colour')
+        subprocess.call([self.exe, '--' + self.allowed_colours[colour]])
+
+
+if __name__ == '__main__':
+    from time import sleep
+    device = DelcomGen2()
+    device.set_light('red')
+    sleep(1)
+    device.set_light('off')
