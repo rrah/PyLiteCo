b463bf9b833d09a9d1bf20dde9e4f202c6377557
Robert Walker <rw776@york.ac.uk>
2015-03-20 15:32:04 +0000

    Tests flashing

--- tests/delcom.py
+++ tests/delcom.py
@@ -6,7 +6,7 @@ Author: Robert Walker <rw776@york.ac.uk> 19/03/15
 try:
     import os
 
-    if os.getcwd().split('\\')[-1] is not 'echolight':
+    if os.getcwd().split('/')[-1] != 'echolight':
         os.chdir('..')
 
     import sys
@@ -25,25 +25,30 @@ try:
     # PATH for packages
     sys.path.append('.')
 
-    import indicators.delcom as delcom
-
     from time import sleep
 
     logging.info('Starting test')
+    
+    import indicators.delcom as delcom
     device = delcom.DelcomGen2()
-
     while True:
-        logging.debug('Red')
-        device.set_light('red')
-        sleep(1)
-        logging.debug('Yellow')
-        device.set_light('yellow')
-        sleep(1)
-        logging.debug('Green')
-        device.set_light('green')
-        sleep(1)
-        logging.debug('Off')
-        device.set_light('off')
-        sleep(1)
+        
+        for colour in device.allowed_colours:
+            logging.info('steady {}'.format(colour))
+            device.set_light(colour)
+            sleep(1)
+        
+        logging.info('flash fast red for 5 seconds')
+        device.set_light_red()
+        device.flashing_start(0.2, 'red')
+        sleep(5)
+        device.flashing_stop()
+        
+        logging.info('flash slow green for 5 seconds')
+        device.set_light_red()
+        device.flashing_start(1, 'green')
+        sleep(5)
+        device.flashing_stop()
+
 except KeyboardInterrupt:
-    pass
+    device.flashing_stop()
