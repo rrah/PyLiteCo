943ec9c86fe028c1ec66c231327671882d87c356
Robert Walker <rw776@york.ac.uk>
2015-04-07 15:34:22 +0100

    and put it in the right one...

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import py2exe
 from distutils.core import setup
 
-setup(console = ['__main__.py'], 
+setup(console = [{"script":"__main__.py", 
+                  "dest_base" : "pyliteco"}], 
       zipfile = None, 
-      options = {"py2exe": {"bundle_files": 1, "dest_base" : "pyliteco"}})
+      options = {"py2exe": {"bundle_files": 1}})
